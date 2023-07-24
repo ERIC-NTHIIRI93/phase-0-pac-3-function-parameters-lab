9af48c03ea11583e3f882126150aed5505a01dbf
ERIC-NTHIIRI93 <137612635+ERIC-NTHIIRI93@users.noreply.github.com>
2023-07-24 14:27:54 +0300

    Add files via upload

---
+++ functions parameters lab.js
@@ -0,0 +1,7 @@
+function sayHelloTo(firstName = "User") {
+    console.log(`Hello, ${firstName}!`);
+  }
+  let name= "ERIC"
+  let language= "SWAHILI"
+  function introductionWithLanguageOptional(${name} and ${language})
+  console.log('name, language') 
